0ecc0a3ba4e8385fa2dbca07b45fc85ffebd0471
suranenggala05 <suranenggalaagung@gmail.com>
2025-07-13 15:53:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #Sidebar Menu
 menu = st.sidebar.selectbox(
     "Pilih menu",
-    ["Beranda","Kalkulator Kimia Dasar","Soal Hots Kimia Dasar","Catatan"]
+    ["Beranda","Kalkulator Kimia Dasar = [Rumus Avogadro] [Rumus Massa Rellative] [Rumus Stoikiometri] [Rumus Termokimia]","Soal Hots Kimia Dasar","Catatan"]
 )
 
 # ---Menu BERANDA ---
@@ -13,24 +13,4 @@ if menu =="Beranda":
     st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgZ14t8HA1se2NSlH_HgONi6kDbZaUahw4c7jakCCoCk12S2gShOu9hdyWP7kLw1pdPOe26UfCi4lwEp3X2P4TxeVkOVxgPoJ9peldCVL7k457LyyhM39t5XH5mPbQovaQbp5yeiufXLfd3lk0v8lR_9_pdj_VHoB11Uo4N1JpFHtNGQjwBz14yjEMHMGlY/s16000-rw/tabel%20periodik%20unsur%20kimia.jpg")
 
 # ---Menu Kalkulator Kimia Dasar---
-    import streamlit as st
-
-# Main page content
-st.markdown("# Kalkulator Kimia Dasar")
-st.sidebar.markdown("# Kalkulator Kimia Dasar")
-    import streamlit as st
-
-    st.markdown("# Rumus Massa Rellative")
-    st.sidebar.markdown("# Rumus Massa Rellative")
-    import streamlit as st
-    
-    st.markdown("# Rumus Stoikiometri")
-    st.sidebar.markdown("# Rumus Stoikiometri")
-    import streamlit as st
-    
-    st.markdown("# Rumus Termokimia")
-    st.sidebar.markdown("# Rumus Termokimia")
-    import streamlit as st
-    
-    st.markdown("# Kesetimbangan Kimia")
-    st.sidebar.markdown("# Kesetimbangan Kimia")
+elif menu == "Kalkulator Kimia Dasar"
