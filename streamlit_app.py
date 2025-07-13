ae6f6dbd67cc0f6a9cad6ff576c3e3832281bda7
suranenggala05 <suranenggalaagung@gmail.com>
2025-07-13 15:26:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,5 @@ menu = st.sidebar.selectbox(
 if menu =="Beranda":
     st.title("Asiknya Meghitung dan Mempermudah dalam Kimia Dasar")
     st.subheader("LPK Kelompok 5 kelas 1B")
-    st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.geografi.org%2F2023%2F07%2Ftabel-periodik-unsur-kimia-dan.html&psig=AOvVaw1vSoW_TsTitTdP-ch0dyyH&ust=1752481246051000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOjo1M2zuY4DFQAAAAAdAAAAABAE",width=400)
+    st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.geografi.org%2F2023%2F07%2Ftabel-periodik-unsur-kimia-dan.html&psig=AOvVaw1vSoW_TsTitTdP-ch0dyyH&ust=1752481246051000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOjo1M2zuY4DFQAAAAAdAAAAABAE")
 
