d153ce2463e1f4131021a666bf2c6e7624dae908
suranenggala05 <suranenggalaagung@gmail.com>
2025-07-13 14:50:56 +0700

    agungs-web_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+#Sidebar Menu
+menu = st.sidebar.selectbox(
+    "Pilih menu"
+    ["Beranda", "Kalkulator Kimia Dasar", "Soal Hots Kimia Dasar"}
+)
+
+# ---Menu BERANDA ---
+if menu =="Beranda"
+    st.title("Asiknya Meghitung dan Mempermudah dalam Kimia Dasar")
+    st.subheader("LPK Kelompok 5 kelas 1B")
+
