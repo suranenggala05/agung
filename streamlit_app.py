2181d17a71924241c0e772b3622247022e294f77
suranenggala05 <suranenggalaagung@gmail.com>
2025-07-16 22:51:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ menu = st.sidebar.selectbox(
 
 # ---Menu BERANDA ---
 if menu =="Beranda":
-    st.title("Kalkulator Kimia Dasar+")
+    st.title("Digital ChemsCalc.id")
     st.subheader("LPK Kelompok 5 kelas 1B")
     st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgZ14t8HA1se2NSlH_HgONi6kDbZaUahw4c7jakCCoCk12S2gShOu9hdyWP7kLw1pdPOe26UfCi4lwEp3X2P4TxeVkOVxgPoJ9peldCVL7k457LyyhM39t5XH5mPbQovaQbp5yeiufXLfd3lk0v8lR_9_pdj_VHoB11Uo4N1JpFHtNGQjwBz14yjEMHMGlY/s16000-rw/tabel%20periodik%20unsur%20kimia.jpg")
     
