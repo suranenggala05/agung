7581972eb9825790056c32d5061e7c1da688f933
suranenggala05 <suranenggalaagung@gmail.com>
2025-07-16 23:13:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,7 @@ menu = st.sidebar.selectbox(
 if menu =="Beranda":
     st.title("Kalkulator Kimia Dasar")
     st.header("Pengertian Kimia")
-    st.writer("" Kimia adalah cabang ilmu pengetahuan alam yang memepelajari tentang struktur, sifat, dan perubahan materi. Ilmu kimia sangat penting dalam kehidupan sehari-hari yang berperan dalam bidang farmasi,lingkungan,pangan, dan energi"")
-    
+   
 #--- Menu Tabel Periodik Unsur ---
 if menu =="Tabel Periodik Unsur":
     st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgZ14t8HA1se2NSlH_HgONi6kDbZaUahw4c7jakCCoCk12S2gShOu9hdyWP7kLw1pdPOe26UfCi4lwEp3X2P4TxeVkOVxgPoJ9peldCVL7k457LyyhM39t5XH5mPbQovaQbp5yeiufXLfd3lk0v8lR_9_pdj_VHoB11Uo4N1JpFHtNGQjwBz14yjEMHMGlY/s16000-rw/tabel%20periodik%20unsur%20kimia.jpg")
