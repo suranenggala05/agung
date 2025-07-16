a51f66df3b5f8059260f09955724cbecb7464282
suranenggala05 <suranenggalaagung@gmail.com>
2025-07-16 22:53:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,15 @@ import streamlit as st
 #Sidebar Menu
 menu = st.sidebar.selectbox(
     "Pilih menu",
-    ["Beranda","Kalkulator Kimia Dasar","Soal-soal Kimia Dasar","Catatan"]
+    ["Beranda","Kalkulator Kimia Dasar","Tabel Periodik Unsur","Soal-soal Kimia Dasar","Catatan"]
 )
 
 # ---Menu BERANDA ---
 if menu =="Beranda":
     st.title("Digital ChemsCalc.id")
     st.subheader("LPK Kelompok 5 kelas 1B")
+    
+#--- Menu Tabel Periodik Unsur
     st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgZ14t8HA1se2NSlH_HgONi6kDbZaUahw4c7jakCCoCk12S2gShOu9hdyWP7kLw1pdPOe26UfCi4lwEp3X2P4TxeVkOVxgPoJ9peldCVL7k457LyyhM39t5XH5mPbQovaQbp5yeiufXLfd3lk0v8lR_9_pdj_VHoB11Uo4N1JpFHtNGQjwBz14yjEMHMGlY/s16000-rw/tabel%20periodik%20unsur%20kimia.jpg")
     
 elif menu == "Kalkulator Kimia Dasar":
